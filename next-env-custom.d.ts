97a9fa58e5f94e4ff9309e9f1996b7d4c7890952
Никита Лесняк <nikitalesnak@MacBook-Pro-Nikita.local>
2022-01-06 22:14:50 +0300

    feat: initial changes

--- next-env-custom.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/types/global" />
-
-// this file is conditionally added/removed to next-env.d.ts
-// if the static image import handling is enabled
